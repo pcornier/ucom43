827913cadacb6630d551a364a4bdf18a34a6ca85
pierco <pier.cornier@gmail.com>
2019-10-16 09:19:58 -0400

    moved plates

--- obj_dir/Vmcu__ver.d
+++
@@ -1,1 +0,0 @@
-obj_dir/Vmcu.cpp obj_dir/Vmcu.h obj_dir/Vmcu.mk obj_dir/Vmcu__Syms.cpp obj_dir/Vmcu__Syms.h obj_dir/Vmcu__ver.d obj_dir/Vmcu_classes.mk  : /usr/bin/verilator_bin /usr/bin/verilator_bin mcu.v 
